30417656d9120f92f8f964c7b43c1befb38ad0d1
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2023-05-09 13:55:24 -0300

    exercício javaScript

---
+++ JS-MODERNO/usandoWebpack/src/index.js
@@ -0,0 +1,5 @@
+import dayjs from "dayjs";
+
+alert(`Hoje é: ${dayjs().format("DD/MM/YYYY")}`);
+
+console.log(`ola node!!!`);
---
+++ JS-MODERNO/usandoWebpack/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path"); // import pacote do NODE
+
+module.exports = {
+  entry: {
+    // Entradas
+    main: "./src/index.js",
+    hello: "./src/hello.js",
+  },
+  mode: "development", // Modelo de arquivo
+  output: {
+    // Saida
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].bundle.min.js", // Nome dos arquivos **([name]. == para quando tiver mais de uma entrada)
+  },
+};
